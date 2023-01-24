32d97c4e96d09d8b037821fcdc9f36ce782305bb
Darshit Dave <darshitdave02@gmail.com>
2023-01-24 13:24:52 +0530

    feat: add hhtp get to understand how to use it

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-var http = require("http");
+var http = require('http');
 const PORT = 3000;
 
 const server = http.createServer(function (req, res) {
-    res.write("Hello World!"); 
-    res.end();
+  res.write('Hello World!'); 
+  res.end();
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is started on port ${PORT}`);
+  console.log(`Server is started on port ${PORT}`);
+
+});
+
+http.get('http://jsonplaceholder.typicode.com/todos/', (res) => {
+ 
+
+  let rawData = '';
+
+  res.on('data', chunk => {
+    rawData += chunk;
+  });
+    
+  res.on('end', () => {
+    const parsedData = JSON.parse(rawData);
+    console.log(parsedData);
+  });
 
 });
