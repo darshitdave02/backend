5fc431c024bc67f9e19ed04cd41007094ff90864
Darshit Dave <darshitdave02@gmail.com>
2023-02-01 21:35:07 +0530

    chore: add basic improvements to the code and updation of libraries

--- index.js
+++ index.js
@@ -1,8 +1,68 @@
 var http = require('http');
-const PORT = 3000;
+const PORT = 6000;
+
+let db = [];
 
 const server = http.createServer(function (req, res) {
-  res.write('Hello World!'); 
+
+  if(req.method == 'GET' && req.url == '/tasks')
+  {
+    res.writeHead(200,{'Content-Type': 'application/json'});
+    res.write(JSON.stringify(db));
+  }
+
+  else if(req.method == 'POST' && req.url == '/tasks')
+  {
+
+    let data = '';
+    req.on('data', res => {
+      data += res;
+    });
+
+    req.on('end', () => {
+      const parsedData = JSON.parse(data);
+      console.log(parsedData);
+      db.push(parsedData);
+    });
+
+    
+    //res.write('Task added!');
+
+  }
+  
+  else if(req.method == 'PATCH' && req.url == '/task/0')
+  {
+    let data = '';
+    req.on('data', res => {
+      data += res;
+    });
+
+    req.on('end', () => {
+      const parsedData = JSON.parse(data);
+      console.log(parsedData);
+      db = db.map((item) => {
+        if(item.name === parsedData.name)
+        {
+          item.isCompleted = parsedData.isCompleted;
+        }
+        return item;
+      });
+    });
+    
+  }
+  else if(req.method == 'DELETE' && req.url == '/task/0')
+  {
+    let id = req.url.split('/')[2];
+
+    db = db.filter((item,idx) => idx!==id );
+
+
+    
+  }
+
+    
+    
+  
   res.end();
 });
 
@@ -11,18 +71,18 @@ server.listen(PORT, () => {
 
 });
 
-http.get('http://jsonplaceholder.typicode.com/todos/', (res) => {
+// http.get('http://jsonplaceholder.typicode.com/todos/', (res) => {
  
 
-  let rawData = '';
+//   let rawData = '';
 
-  res.on('data', chunk => {
-    rawData += chunk;
-  });
+//   res.on('data', chunk => {
+//     rawData += chunk;
+//   });
     
-  res.on('end', () => {
-    const parsedData = JSON.parse(rawData);
-    console.log(parsedData);
-  });
+//   res.on('end', () => {
+//     const parsedData = JSON.parse(rawData);
+//     console.log(parsedData);
+//   });
 
-});
+// });
